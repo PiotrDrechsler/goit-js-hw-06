01254a1800c6c8196ced270d87ffdd777f7e2848
PiotrDrechsler <pdrech@gmail.com>
2022-09-26 21:44:38 +0200

    Update task-09.js
    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyColor = document.querySelector("body");
+const textColor = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
+const changeColor = () => {
+  const rngColor = getRandomHexColor();
+  bodyColor.style.backgroundColor = `${rngColor}`;
+  textColor.textContent = `${rngColor}`;
+};
+
+button.addEventListener("click", changeColor);
